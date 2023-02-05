a6c5240440900522e7ca484114be56026053d27c
lalakagawa <lalakagawa@hotmail.com>
2023-02-05 15:15:33 -0300

    Adicionado as entidades de order

---
+++ src/main/java/br/com/fiap/sportconnection/ecommerce/dto/OrderDTO.java
@@ -0,0 +1,15 @@
+package br.com.fiap.sportconnection.ecommerce.dto;
+
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record OrderDTO (
+        Long id,
+        String description,
+        BigDecimal total,
+        BigDecimal discount,
+        Long costumerId,
+        List<ProductQuantityItemDTO> products
+) {
+}
---
+++ src/main/java/br/com/fiap/sportconnection/ecommerce/entity/OrderProductEntity.java
@@ -0,0 +1,53 @@
+package br.com.fiap.sportconnection.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "order_product")
+public class OrderProductEntity {
+
+
+    public OrderProductEntity(OrderEntity order, ProductEntity product) {
+        this.order = order;
+        this.product = product;
+    }
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(unique = true, nullable = false)
+    private Long orderProductId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private OrderEntity order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private ProductEntity product;
+
+    private Long quantity;
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof OrderProductEntity)) return false;
+        OrderProductEntity that = (OrderProductEntity) obj;
+        return Objects.equals(product.getName(), that.product.getName()) &&
+                Objects.equals(product.getId(), that.product.getId()) &&
+                Objects.equals(product.getCode(), that.product.getCode()) &&
+                Objects.equals(order.getId(), that.order.getId()) &&
+                Objects.equals(order.getTotal(), that.order.getTotal()) &&
+                Objects.equals(quantity, that.quantity);
+    }
+}
